0e8ef8b0c590fd2ca05455fe45800a923630b3d7
Ali Rezaei <ali.rezaei0021@gmail.com>
2021-09-16 20:36:42 +0430

    faq add toggle & anime

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,57 +1,57 @@
 import React from "react";
 import { About } from "../Style";
 import styled from "styled-components";
-
+import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
 const FaqSection = () => {
     return (
         <Faq>
             <h2>
                 Any Questions <span>FAQ</span>
             </h2>
-            <div className="question">
-                <h4>How Do I Start?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Asperiores doloremque nam
-                    </p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Daily Schedule?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Asperiores doloremque nam
-                    </p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Diferrent Payment Methods?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Asperiores doloremque nam
-                    </p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>What Products do you offer?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Asperiores doloremque nam
-                    </p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
+            <AnimateSharedLayout>
+                <Toggle title="How Do I Start?">
+                    {/* <h4>How Do I Start?</h4> */}
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Asperiores doloremque nam
+                        </p>
+                    </div>
+                </Toggle>
+                <Toggle title="Daily Schedule?">
+                    {/* <h4>Daily Schedule?</h4> */}
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Asperiores doloremque nam
+                        </p>
+                    </div>
+                </Toggle>
+                <Toggle title="Diferrent Payment Methods?">
+                    {/* <h4>Diferrent Payment Methods?</h4> */}
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Asperiores doloremque nam
+                        </p>
+                    </div>
+                </Toggle>
+                <Toggle title="What Products do you offer?">
+                    {/* <h4>What Products do you offer?</h4> */}
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Asperiores doloremque nam
+                        </p>
+                    </div>
+                </Toggle>
+            </AnimateSharedLayout>
         </Faq>
     );
 };
