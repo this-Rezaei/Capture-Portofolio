43a9388208b212b22de101afe25241b3a91c7320
Ali Rezaei <ali.rezaei0021@gmail.com>
2021-09-16 16:18:12 +0430

    Aout Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,38 +2,14 @@ import React from "react";
 import home1 from "../img/home1.png";
 import { About, Image, Description, Hide } from "../Style";
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../Pages/animation";
 const AboutSection = () => {
-    const titleAnim = {
-        hidden: { opacity: 0 },
-        show: { opacity: 1, transition: { duration: 2 } },
-    };
-    const container = {
-        hidden: { x: 100 },
-        show: {
-            x: 0,
-            transition: {
-                duration: 0.75,
-                ease: "easeOut",
-                staggerChildren: 0.75,
-                // when: "afterChildren",
-            },
-        },
-    };
     return (
         <About>
             <Description>
-                <motion.div
-                    variants={container}
-                    initial="hidden"
-                    animate="show"
-                    className="title"
-                >
+                <motion.div>
                     <Hide>
-                        <motion.h2
-                            variants={titleAnim}
-                            animate="show"
-                            initial="hidden"
-                        >
+                        <motion.h2 variants={titleAnim}>
                             We Work to make
                         </motion.h2>
                     </Hide>
@@ -47,16 +23,16 @@ const AboutSection = () => {
                     </Hide>
                 </motion.div>
 
-                <p>
+                <motion.p variants={fade}>
                     Contact us for any photography or videography ideas that you
                     have. We have professionals with amazing skills to help you
                     achieve it.
-                </p>
-                <button>Contact Us</button>
+                </motion.p>
+                <motion.button variants={fade}>Contact Us</motion.button>
             </Description>
 
             <Image>
-                <img src={home1} alt="Photo" />
+                <motion.img variants={photoAnim} src={home1} alt="Photo" />
             </Image>
         </About>
     );
